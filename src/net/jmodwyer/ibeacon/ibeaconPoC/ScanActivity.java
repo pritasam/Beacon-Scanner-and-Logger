cecf8defc1c6b73ebe846d99f1372e7b5b8da79a
Justin O'Dwyer <justin@justins-mbp.objectivity.co.uk>
2014-03-11 16:05:21 +0000

    Tidied up unused import.

--- src/net/jmodwyer/ibeacon/ibeaconPoC/ScanActivity.java
+++ src/net/jmodwyer/ibeacon/ibeaconPoC/ScanActivity.java
@@ -8,7 +8,6 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.RemoteException;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
